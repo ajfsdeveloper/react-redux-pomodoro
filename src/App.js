5f9c95326d0bba4a57ed9b936be8b20456b9f6db
Amol Jagtap <ajfsdeveloper@gmail.com>
2018-09-22 11:48:54 +0530

    Create and Render TimerDisplay component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react'
+import TimerDisplay from './components/TimerDisplay'
 
 class App extends Component {
   render() {
     return (
       <div>
         <h1>Hey There!!</h1>
+        <TimerDisplay />
       </div>
     )
   }
---
+++ src/components/TimerDisplay.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const TimerDisplay = () => {
+  return <div>20 : 00</div>
+}
+
+export default TimerDisplay
