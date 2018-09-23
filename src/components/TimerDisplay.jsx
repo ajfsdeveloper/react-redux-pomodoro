bd0d8bab1cbe3bbc74d1aa6594743306a8d4959d
Amol Jagtap <ajfsdeveloper@gmail.com>
2018-09-23 20:52:07 +0530

    CSS changes

--- src/components/TimerDisplay.jsx
+++ src/components/TimerDisplay.jsx
@@ -4,8 +4,12 @@ const TimerDisplay = (props) => {
   return <div className="container section">
     <div className="row">
       <div className="col s6 offset-s3">
-        <div className="card-panel teal z-depth-5">
-          <h1 className="center-align blue-grey-text text-darken-4">{props.displayTime}</h1>
+        <div className="card-panel teal z-depth-5" style={{ borderRadius: '99px' }}>
+          <h1
+            className="center-align blue-grey-text text-darken-4"
+            style={{ fontWeight: 'bold', fontSize: '10rem' }}>
+            {props.displayTime}
+          </h1>
         </div>
       </div>
     </div>
