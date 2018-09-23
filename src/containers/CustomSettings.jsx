49d75efdd336d0764a0481fb23fc5c209a1f641a
Amol Jagtap <ajfsdeveloper@gmail.com>
2018-09-23 16:32:48 +0530

    Fix CSS issue with range slider

--- src/containers/CustomSettings.jsx
+++ src/containers/CustomSettings.jsx
@@ -4,14 +4,24 @@ class CustomSettings extends Component {
   render() {
     return (
       <div className="container">
-        <ul className="collapsible">
+        <ul className="collapsible z-depth-5">
           <li>
-            <div className="collapsible-header">
-              Set Custom Time
-          </div>
+            <div className="collapsible-header cyan darken-3">
+              <span className="flow-text">Set Custom Time</span>
+            </div>
             <div className="collapsible-body">
-              Some settings
-          </div>
+              <form action="#">
+                <p className="range-field">
+                  <label htmlFor="minute">Minutes:</label>
+                  <input type="range" id="minute" min="0" max="60" />
+                </p>
+
+                <p className="range-field">
+                  <label htmlFor="second">Seconds:</label>
+                  <input type="range" id="second" min="0" max="60" />
+                </p>
+              </form>
+            </div>
           </li>
         </ul>
         <div className="divider"></div>
