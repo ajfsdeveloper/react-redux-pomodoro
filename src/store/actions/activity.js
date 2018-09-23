87bdfb761ac1f85d8fe96bde4b608116c57831d5
Amol Jagtap <ajfsdeveloper@gmail.com>
2018-09-23 19:55:57 +0530

    Refactored activity and timer action creators

--- src/store/actions/activity.js
+++ src/store/actions/activity.js
@@ -6,32 +6,12 @@ import {
   COMPLETE_POMODORO
 } from './actionTypes'
 
-export const startPomodoro = () => {
-  return {
-    type: START_POMODORO
-  }
-}
+export const startPomodoro = () => ({ type: START_POMODORO })
 
-export const stopPomodoro = () => {
-  return {
-    type: STOP_POMODORO
-  }
-}
+export const stopPomodoro = () => ({ type: STOP_POMODORO })
 
-export const pausePomodoro = () => {
-  return {
-    type: PAUSE_POMODORO
-  }
-}
+export const pausePomodoro = () => ({ type: PAUSE_POMODORO })
 
-export const resumePomodoro = () => {
-  return {
-    type: RESUME_POMODORO
-  }
-}
+export const resumePomodoro = () => ({ type: RESUME_POMODORO })
 
-export const completePomodoro = () => {
-  return {
-    type: COMPLETE_POMODORO
-  }
-}
+export const completePomodoro = () => ({ type: COMPLETE_POMODORO })
--- src/store/actions/timer.js
+++ src/store/actions/timer.js
@@ -18,79 +18,69 @@ import {
 
 let timerClock = null
 
-export const setDefaultTime = (defaultTimeString) => {
-  return {
-    type: SET_DEFAULT_TIME,
-    defaultTime: defaultTimeString
+export const setDefaultTime = (defaultTimeString) => ({
+  type: SET_DEFAULT_TIME,
+  defaultTime: defaultTimeString
+})
+
+export const updateDisplayTime = (updatedDisplayTimeString) => ({
+  type: UPDATE_DISPLAY_TIME,
+  updatedDisplayTime: updatedDisplayTimeString
+})
+
+export const startTimer = activity => (dispatch, getState) => {
+
+  let currentDisplayTime = getState().timer.displayTime
+  let defaultTime = getState().timer.defaultTime
+
+  if (checkValidTime(currentDisplayTime) === false) {
+    dispatch(updateDisplayTime(defaultTime))
   }
-}
 
-export const updateDisplayTime = (updatedDisplayTimeString) => {
-  return {
-    type: UPDATE_DISPLAY_TIME,
-    updatedDisplayTime: updatedDisplayTimeString
+  if (activity === 'START') {
+    dispatch(startPomodoro())
+  } else {
+    dispatch(resumePomodoro())
   }
-}
 
-export const startTimer = (activity) => {
-  return (dispatch, getState) => {
+  timerClock = setInterval(() => {
+    currentDisplayTime = getState().timer.displayTime
+    const updatedDisplayTimeString = getUpdatedDisplayTimeString(currentDisplayTime)
 
-    let currentDisplayTime = getState().timer.displayTime
-    let defaultTime = getState().timer.defaultTime
+    dispatch(updateDisplayTime(updatedDisplayTimeString))
 
-    if (checkValidTime(currentDisplayTime) === false) {
-      dispatch(updateDisplayTime(defaultTime))
+    if (updatedDisplayTimeString === '00:00') {
+      dispatch(stopTimer('COMPLETE'))
     }
-
-    if (activity === 'START') {
-      dispatch(startPomodoro())
-    } else {
-      dispatch(resumePomodoro())
-    }
-
-    timerClock = setInterval(() => {
-      currentDisplayTime = getState().timer.displayTime
-      const updatedDisplayTimeString = getUpdatedDisplayTimeString(currentDisplayTime)
-
-      dispatch(updateDisplayTime(updatedDisplayTimeString))
-
-      if (updatedDisplayTimeString === '00:00') {
-        dispatch(stopTimer('COMPLETE'))
-      }
-    }, 1000)
-  }
+  }, 1000)
 }
 
-export const pauseTimer = () => {
+export const pauseTimer = () => (dispatch, getState) => {
+
   clearInterval(timerClock)
 
-  return (dispatch, getState) => {
+  let currentDisplayTime = getState().timer.displayTime
 
-    let currentDisplayTime = getState().timer.displayTime
+  if (checkValidTime(currentDisplayTime) === false) {
+    return
+  }
+
+  dispatch(pausePomodoro())
+}
+
+export const stopTimer = activity => (dispatch, getState) => {
+
+  clearInterval(timerClock)
+
+  if (activity === 'COMPLETE') {
+    dispatch(completePomodoro())
+  } else {
+    const currentDisplayTime = getState().timer.displayTime
 
     if (checkValidTime(currentDisplayTime) === false) {
       return
     }
 
-    dispatch(pausePomodoro())
-  }
-}
-
-export const stopTimer = (activity) => {
-  clearInterval(timerClock)
-
-  return (dispatch, getState) => {
-
-    if (activity === 'COMPLETE') {
-      dispatch(completePomodoro())
-    } else {
-      let currentDisplayTime = getState().timer.displayTime
-
-      if (checkValidTime(currentDisplayTime) === false) {
-        return
-      }
-
-      dispatch(stopPomodoro())
-    }
+    dispatch(stopPomodoro())
   }
 }
